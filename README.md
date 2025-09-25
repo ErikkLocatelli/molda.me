7472a292fcdb1acc1b1773a305e0e52849576269
Erik <erikklocatelli@gmail.com>
2025-09-24 22:06:42 -0300

    add: adiciona README e entrega versão final do projeto

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# molda.me
+# Cadastro de Medidas 📏 
+Desafio prático do processe seletivo para Estágio em desenvolvimento na Molde.me. 
 
+Objetivo da aplicação é permitir que o usuário adicione, visualize e remova medidas em uma tebela simples e responsiva.
+
+
+## Tecnologias utilizadas 
+
+- JavaScript. 
+- HTML5. 
+- CSS3. 
+
+## Estrutura do projeto 
+
+- index.html -> página principal com formulário e tabela.
+- style.css -> estilos da aplicação.
+- script.js -> lógica da aplicação (adicionar, remover e salvar medidas).
+
+## Funcionalidades 
+
+- Adicionar uma medida informando:
+
+    - Nome da medida (ex: "Cintura", "Quadril").
+
+    - Valor (número).
+
+    - Unidade (cm, mm, pol etc.).
+
+- Visualizar todas as medidas cadastradas em uma tabela.
+
+- Remover medidas individualmente.
+
+- Responsividade para uso em desktop e mobile.
+
+## Como executar 
+
+1. Baixe ou clone este repositório.
+
+2. Abra o arquivo index.html diretamente em seu navegador.
+
+3. Não é necessário instalar nada — a aplicação roda apenas com HTML, CSS e JS.
--- app/script.js
+++ app/script.js
@@ -1,34 +1,37 @@
 const form = document.querySelector('[data-form="form"]')
 const tabela = document.querySelector('[data-tabela="medidas"]')
 
-
 function insereMedida(nome, valor, medida) {
-    const tr = document.createElement('tr')
+    const tr = document.createElement('tr') // cria o elemento onde serão inseridos os dados pelo usuário
+    tr.classList.add('tabela-infos') // adiciona um estilo pré-definido para o elemento
 
-    tr.innerHTML= `
+    tr.innerHTML= `                      
         <th>${nome}</th>
-        <th>${valor}</th>
+        <th>${valor}</th>  
         <th>${medida}</th>
-        <td><button class="remover">Remover</button></td>
+        <th><button class="remover">Remover</button></th>
     `
 
-    // const remover = document.querySelector("remover") 
-    // remover.addEventListener("click", (e) => {
-    //     e.remove(tr.currentTarget)
-    // } )
-
+    const remover = tr.querySelector(".remover") 
+    remover.addEventListener("click", () => {
+         tr.remove()
+     } )
     tabela.appendChild(tr)
-
 }
 
+// funcao que recebe os dados atraves do submit do form
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
+
+    const top = document.querySelector('[data-tabela="top"]')
+    top.classList.add('ativo') // adiciona a classe ativo para que o cabeçalho das infos apareça quando há dados a serem mostrados
 
     const nome = document.getElementById("nome").value
     const valor = document.getElementById("valor").value
     const unidade = document.getElementById("unidade").value
     
-    insereMedida(nome, valor, unidade)
+    insereMedida(nome, valor, unidade) // atribue os dados na função de criação de tabela
+    form.reset(); //reseta o form após envio
 })
 
